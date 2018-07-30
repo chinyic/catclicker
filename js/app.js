4c51a5bcb46e8f81f18f2fab32c0c4460fcaf02b
Chinyi C <chinyi.cc@gmail.com>
2018-07-29 22:26:59 -0700

    start JS for catImg

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
 //insert JS for event listener when cat clicked
 //insert JS for increment move
+var catImg = document.getElementById("catImg1");
+catImg.addEventListener("click", () => {modifyText)
+function modifyText(){
+  var clickedText = document.getElementById("clickText1");
+  for (let i=0; i<catImg1.length; i++){
+
+  }
+}
