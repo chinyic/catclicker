243003b5e57edae062a451c849bdde8d52907ff6
Chinyi C <chinyi.cc@gmail.com>
2018-07-30 21:50:11 -0700

    click counter

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 //insert JS for event listener when cat clicked
 //insert JS for increment move
-var catImg = document.getElementById("catImg1");
-catImg.addEventListener("click", () => {modifyText)
-function modifyText(){
-  var clickedText = document.getElementById("clickText1");
-  for (let i=0; i<catImg1.length; i++){
+const catImg = document.getElementById("catImg1");
+let clickCount = 0;
+const clickCounter = document.getElementById("clicks");
 
-  }
-}
+catImg.addEventListener("click", changeNum, false);
+
+function changeNum(e){
+    clickCount++;
+    clickCounter.innerHTML = clickCount;
+  };
