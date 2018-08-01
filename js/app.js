7acc9f5ae83988618de7dd12500354d6ff66936b
Chinyi C <chinyi.cc@gmail.com>
2018-08-01 15:10:10 -0700

    catimg2 - click counter not working

--- js/app.js
+++ js/app.js
@@ -3,9 +3,10 @@
 const cat1 = document.getElementById("catImg1");
 const cat2 = document.getElementById("catImg2");
 let clickCount = 0;
-const clickCounter = document.getElementById("clicks");
+const clickCounter = document.getElementById('click1');
 
 cat1.addEventListener("click", changeNum, false);
+cat2.addEventListener("click", changeNum, false);
 
 function changeNum(){
     clickCount++;
@@ -14,4 +15,29 @@ function changeNum(){
 
 //store cat name in JS variable, cat name above cat image
 
-const allCats = [""];
+/*
+var allCats = [{
+name: 'Poplinre',
+src: 'images/cat1.jpg',
+clickCount: 0,
+},
+{
+  name: 'Cat2',
+  src: 'images/cat2.jpg',
+  clickCount: 0
+}
+];
+
+var selectedCat =
+document.body.appendChild(allCats);
+
+let clickCount = 0;
+const clickCounter = document.getElementById("clicks");
+
+cat1.addEventListener("click", changeNum, false);
+
+function changeNum(){
+    clickCount++;
+    clickCounter.innerHTML = clickCount;
+  };
+*/
